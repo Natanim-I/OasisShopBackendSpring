36c5c870711d2187411f61f9777642a12f699e89
Natanim Issa <natanimissa@gmail.com>
2026-05-24 13:47:13 -0400

    added fetching product by id

--- src/main/java/com/oasis/OasisShop/controller/ProductsController.java
+++ src/main/java/com/oasis/OasisShop/controller/ProductsController.java
@@ -3,6 +3,8 @@ package com.oasis.OasisShop.controller;
 import com.oasis.OasisShop.model.Product;
 import com.oasis.OasisShop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,12 +18,30 @@ public class ProductsController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public List<Product> allProducts(){
-        return productService.getAllProducts();
+    public ResponseEntity<List<Product>> allProducts(){
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") int id){
+        Product product = productService.getProductById(id);
+        if(product != null)
+            return new ResponseEntity<>(productService.getProductById(id), HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/product")
     public Product addProduct(@RequestBody Product product){
         return productService.addProduct(product);
+    }
+
+    @PutMapping("/product")
+    public Product updateProduct(@RequestBody Product product){
+        return productService.updateProduct(product);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public void deleteProduct(@PathVariable("id") int id){
+        productService.deleteProduct(id);
     }
 }
--- src/main/java/com/oasis/OasisShop/service/ProductService.java
+++ src/main/java/com/oasis/OasisShop/service/ProductService.java
@@ -17,7 +17,19 @@ public class ProductService {
         return productsRepo.findAll();
     }
 
+    public Product getProductById(int id) {
+        return productsRepo.findById(id).orElse(null);
+    }
+
     public Product addProduct(Product product){
         return productsRepo.save(product);
+    }
+
+    public Product updateProduct(Product product) {
+        return productsRepo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        productsRepo.deleteById(id);
     }
 }
