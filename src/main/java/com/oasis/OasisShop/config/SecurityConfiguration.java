a960180b5240afb81ced8345a36af7bab7d2a1b1
Natanim Issa <natanimissa@gmail.com>
2026-06-04 20:13:12 -0400

    added jwt filter to user jwt token

--- src/main/java/com/oasis/OasisShop/config/SecurityConfiguration.java
+++ src/main/java/com/oasis/OasisShop/config/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,14 +23,17 @@ public class SecurityConfiguration {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private JwtFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(request -> request.requestMatchers("/api/register", "/api/login").permitAll().anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
--- src/main/java/com/oasis/OasisShop/service/JwtService.java
+++ src/main/java/com/oasis/OasisShop/service/JwtService.java
@@ -1,8 +1,18 @@
 package com.oasis.OasisShop.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public class JwtService {
 
     public String generateToken(String username) {
         return "";
+    }
+
+    public String extractUsername(String token) {
+        return "";
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        return true;
     }
 }
